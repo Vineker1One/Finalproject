70f62230a9783d4ad7e187f7a75ab3f603fde5bf
Ivan <ikorovyanskiy04@mail.ru>
2022-11-24 23:47:25 +0300

    add FirstArray

--- Final Project/kod/Program.cs
+++ Final Project/kod/Program.cs
@@ -5,3 +5,18 @@
 
     Console.WriteLine();
 }
+
+string[] FirstArray() 
+{
+  Console.Write("Напишите какой длины создаётся массив: ");
+  int size = Convert.ToInt32(Console.ReadLine());
+  string[] array = new string[size];
+  Console.WriteLine("Создаём массив: ");
+  for (int i = 0; i < size; i++)
+  {
+    Console.Write($"Введите значение {i + 1} элемента массива: ");
+    array[i] = Console.ReadLine();
+  }
+  Console.WriteLine("Готово!");
+  return array;
+}
